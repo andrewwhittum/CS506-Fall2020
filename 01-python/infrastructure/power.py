c179baae56a9dabb0753dc95957cef8edfea56ba
Andrew Whittum <andrewwhittum@gmail.com>
2020-09-12 20:03:53 -0400

    drew the power plant

--- 01-python/infrastructure/power.py
+++ 01-python/infrastructure/power.py
@@ -1,3 +1,18 @@
 def draw_power_plant():
-    print("power plant not found")
+    print("    (  )         ( )  ( )  ( )")
+    print("   (    )       ( )  ( )  ( )")
+    print("  (      )     ( )  ( )  ( )")
+    print(" (        )   ( )  ( )  ( )")
+    print("\          /  | |  | |  | |   ")
+    print(" \        /   | |  | |  | |   ")
+    print("  \      /    | |  | |  | |   ")
+    print("  /      \    | |  | |  | |   ")
+    print(" /        \   | |  | |  | |   ")
+    print("/__________\__|_|__|_|__|_|___")
+    print("|                            |")
+    for i in range(height):
+        print("|                            |")
+    print("|                            |")
+    print("|     _                      |")
+    print("|    | |                     |") 
     return
