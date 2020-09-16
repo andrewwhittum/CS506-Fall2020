d0bc89fcf3fd941745fb63b0fdb63a07922d98be
Andrew Whittum <andrewwhittum@gmail.com>
2020-09-15 23:17:25 -0400

    added a variable for powerplant height

--- 01-python/infrastructure/power.py
+++ 01-python/infrastructure/power.py
@@ -1,4 +1,4 @@
-def draw_power_plant():
+def draw_power_plant(height = 1):
     print("    (  )         ( )  ( )  ( )")
     print("   (    )       ( )  ( )  ( )")
     print("  (      )     ( )  ( )  ( )")
@@ -10,6 +10,8 @@ def draw_power_plant():
     print(" /        \   | |  | |  | |   ")
     print("/__________\__|_|__|_|__|_|___")
     print("|                            |")
+    for i in range(height):
+            print("|                            |")
     print("|                            |")
     print("|     _                      |")
     print("|    | |                     |") 
